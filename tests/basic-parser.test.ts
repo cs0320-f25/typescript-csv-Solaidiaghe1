a445fefd6e6199073b2cdd430f2f01eecae893de
Solaidiaghe1 <solaidiaghe365@gmail.com>
2025-09-12 18:14:38 -0400

    updated the csv file after slight error

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -32,7 +32,7 @@ test("my own csv first row equals header", async () => {
 });
 
 test("my own csv second row equals John, Doe", async () => {
-  const results = await parseCSV(testingPath, null)
+  const results = await parseCSV(testingPath)
   expect(results[1]).toEqual(["John, Doe", "20", "A"])
 });
 
